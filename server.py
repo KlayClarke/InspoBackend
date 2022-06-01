9d4d2dbfb95b0cd43036cb775af1c00318bd6427
Klay Clarke <clarkeklay@gmail.com>
2022-05-31 23:15:37 -0400

    add code for missing request (request for posting video)

--- server.py
+++ server.py
@@ -47,6 +47,32 @@ async def get_videos():
         database=POSTGRES_DB, user=POSTGRES_USER, password=POSTGRES_PASSWORD, host={POSTGRES_HOST},
     )
     cur = conn.cursor()
+    cur.execute("SELECT * FROM videos ORDER BY id DESC")
+    rows = cur.fetchall()
+    formatted_videos = []
+    for row in rows:
+        formatted_videos.append(
+            VideoModel(id=row[0], video_title=row[1], video_url=row[2])
+        )
+    cur.close()
+    conn.close()
+    return formatted_videos
+
+
+@app.post('/videos', status_code=201)
+async def add_video(file: UploadFile):
+    # upload file to AWS S3
+    s3 = boto3.resource("s3")
+    bucket = s3.Bucket(S3_BUCKET_NAME)
+    bucket.upload_fileobj(file.file, file.filename,
+                          ExtraArgs={"ACL": "public-read"})
+    uploaded_file_url = f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/{file.filename}"
+    # store url in postgres database
+    # connect to database
+    conn = psycopg2.connect(
+        database=POSTGRES_DB, user=POSTGRES_USER, password=POSTGRES_PASSWORD, host={POSTGRES_HOST},
+    )
+    cur = conn.cursor()
     cur.execute(
         f"INSERT INTO videos (video_title, video_url) VALUES ('{file.filename}', '{uploaded_file_url}')"
 
